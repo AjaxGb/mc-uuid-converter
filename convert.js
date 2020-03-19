893edd1dab7c3364902654833ad0bb2112cd1fa9
Sol Toder <ajaxgb@gmail.com>
2020-03-18 22:46:30 -0400

    Better formatting, gen random, load from hash

--- convert.js
+++ convert.js
@@ -1,12 +1,15 @@
-const uuid = new DataView(new ArrayBuffer(16));
+const uuidBytes = new Uint8Array(16);
+const uuid = new DataView(uuidBytes.buffer);
 
-const views = {
+const uuidViews = {
 	hex: {
-		inputs: {
-			hex: document.getElementById('hex'),
+		inputs: [
+			document.getElementById('hex')
+		],
+		constants: {
 			groupSizes: [8, 4, 4, 4, 12],
 		},
-		parse: ({hex, groupSizes}) => {
+		parse: ([hex], {groupSizes}) => {
 			if (!hex.validity.valid) return;
 			const hexText =  hex.value.includes('-')
 				? hex.value.trim()
@@ -17,7 +20,7 @@ const views = {
 			uuid.setBigUint64(0, BigInt('0x' + hexText.substring(0, 16)), false);
 			uuid.setBigUint64(8, BigInt('0x' + hexText.substring(16)), false);
 		},
-		unparse: ({hex, groupSizes}) => {
+		unparse: ([hex], {groupSizes}) => {
 			const hexText =
 				uuid.getBigUint64(0, false).toString(16).padStart(16, '0') +
 				uuid.getBigUint64(8, false).toString(16).padStart(16, '0');
@@ -31,24 +34,26 @@ const views = {
 		},
 	},
 	halves: {
+		allowSplitPaste: true,
 		inputs: [
-			document.getElementById('least'),
 			document.getElementById('most'),
+			document.getElementById('least'),
 		],
-		parse: ([least, most]) => {
-			if (least.validity.valid) {
-				uuid.setBigInt64(8, BigInt(least.value), false);
-			}
+		parse: ([most, least]) => {
 			if (most.validity.valid) {
 				uuid.setBigInt64(0, BigInt(most.value), false);
 			}
+			if (least.validity.valid) {
+				uuid.setBigInt64(8, BigInt(least.value), false);
+			}
 		},
-		unparse: ([least, most]) => {
-			least.value = uuid.getBigInt64(8, false);
+		unparse: ([most, least]) => {
 			most.value = uuid.getBigInt64(0, false);
+			least.value = uuid.getBigInt64(8, false);
 		},
 	},
 	array: {
+		allowSplitPaste: true,
 		inputs: Array.from({ length: 4 },
 			(_, i) => document.getElementById('array' + i)),
 		parse: array => {
@@ -66,39 +71,77 @@ const views = {
 	},
 };
 
-function updateViews(changedView) {
-	views[changedView].parse(views[changedView].inputs);
-	for (const viewId in views) {
-		if (viewId !== changedView) {
-			views[viewId].unparse(views[viewId].inputs);
+function updateUUIDViews(changedViewId) {
+	const changedView = uuidViews[changedViewId];
+	if (changedView) {
+		changedView.parse(changedView.inputs, changedView.constants || {});
+	}
+	for (const viewId in uuidViews) {
+		if (viewId !== changedViewId) {
+			const view = uuidViews[viewId];
+			view.unparse(view.inputs, view.constants || {});
 		}
 	}
 }
 
+function generateRandomUUID() {
+	crypto.getRandomValues(uuidBytes);
+	// Set version to 4 (random)
+	uuidBytes[6] = (uuidBytes[6] & 0x0f) | (4 << 4);
+	// Set variant to 2 (RFC4122)
+	uuidBytes[8] = (uuidBytes[8] & 0x3f) | 0xc0;
+	updateUUIDViews();
+}
+
+function loadUUIDFromHash() {
+	const text = location.hash.substring(1);
+	if (!text) return;
+
+	const textGroups = text.split(',');
+	for (const viewId in uuidViews) {
+		const view = uuidViews[viewId];
+		if (view.inputs.length === textGroups.length) {
+			for (let i = 0; i < textGroups.length; i++) {
+				view.inputs[i].value = textGroups[i];
+			}
+			updateUUIDViews(viewId);
+			return;
+		}
+	}
+	// No matching length, fall back to hex
+	uuidViews.hex.inputs[0].value = text;
+	updateUUIDViews('hex');
+}
+
+document.getElementById('gen-random').addEventListener('click', generateRandomUUID);
+
 document.addEventListener('input', e => {
 	const viewId = e.target.closest('.row').dataset.viewId;
 	if (viewId) {
-		updateViews(viewId);
+		updateUUIDViews(viewId);
 	}
 });
 
 document.addEventListener('paste', e => {
-	const rowData = e.target.closest('.row').dataset;
-	const viewId = rowData.viewId;
-	if (viewId && 'splitPaste' in rowData) {
-		for (const item of e.clipboardData.items) {
-			if (item.kind === 'string' && item.type.startsWith('text/plain')) {
-				item.getAsString(text => {
-					const numbers = text.match(/[+-]?\d+/g);
-					if (numbers && numbers.length === views[viewId].inputs.length) {
-						for (let i = 0; i < numbers.length; i++) {
-							views[viewId].inputs[i].value = numbers[i];
-						}
-						updateViews(viewId);
+	const viewId = e.target.closest('.row').dataset.viewId;
+	const view = uuidViews[viewId];
+	if (!view || !view.allowSplitPaste) return;
+	
+	for (const item of e.clipboardData.items) {
+		if (item.kind === 'string' && item.type.startsWith('text/plain')) {
+			item.getAsString(text => {
+				const numbers = text.match(/[+-]?\d+/g);
+				if (numbers && numbers.length === view.inputs.length) {
+					for (let i = 0; i < numbers.length; i++) {
+						view.inputs[i].value = numbers[i];
 					}
-				});
-				break;
-			}
+					updateUUIDViews(viewId);
+				}
+			});
+			break;
 		}
 	}
 });
+
+window.addEventListener('hashchange', loadUUIDFromHash);
+loadUUIDFromHash();
